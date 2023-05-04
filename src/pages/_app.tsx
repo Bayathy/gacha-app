f47c3627346052dc707a7ee50695ef908fe4148c
Bayathy <misosoup0321@icloud.com>
2023-05-05 04:23:34 +0900

    init commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// eslint-disable-next-line import/named
+import { MantineProvider } from '@mantine/core';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app';
+
+export default function App(properties: AppProps) {
+  const { Component, pageProps } = properties;
+
+  return (
+      <>
+        <Head>
+          <title>Page title</title>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        </Head>
+
+        <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{
+              /** Put your mantine theme override here */
+              colorScheme: 'light',
+            }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </>
+  );
 }
