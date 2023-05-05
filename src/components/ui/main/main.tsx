0938d2779f7797cac53f202ed5c9d55ccb98bd0f
Bayathy <misosoup0321@icloud.com>
2023-05-05 22:26:38 +0900

    feat: style main

--- src/components/ui/main/main.tsx
+++ src/components/ui/main/main.tsx
@@ -1,11 +1,19 @@
-import { Box, Text } from '@mantine/core'
+import { Box, Button, Divider, Group, Stack, Text } from '@mantine/core'
 
 export function Main() {
   return (
-    <main>
-      <Box>
-        <Text>Main</Text>
-      </Box>
+    <main className={'m-auto flex h-full w-full max-w-2xl items-center justify-center'}>
+      <Stack justify={'stretch'} className={'w-full'}>
+        <Box className={'items-center justify-center rounded-md p-8 shadow-md'}>
+          <Text className={'line-clamp-3 text-3xl'}>Product</Text>
+          <Divider py={'md'} />
+          <Text className={'text-2xl'}>番号: 1I01</Text>
+        </Box>
+        <Group grow spacing={'md'}>
+          <Button>抽選する</Button>
+          <Button>次へ</Button>
+        </Group>
+      </Stack>
     </main>
   )
 }
