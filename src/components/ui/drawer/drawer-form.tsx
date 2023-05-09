ddb390b10f5162d833a58533af28e5d3caefa7f2
Bayathy <misosoup0321@icloud.com>
2023-05-09 09:13:03 +0900

    feat: create number of poople Input

--- src/components/ui/drawer/drawer-form.tsx
+++ src/components/ui/drawer/drawer-form.tsx
@@ -1,10 +1,35 @@
 import { Button, Divider, FileButton, NumberInput, Stack } from '@mantine/core'
+import { useForm } from '@mantine/form'
+
+type DrawerForm = {
+  peoples: {
+    '1I': number
+    '2I': number
+    '3I': number
+    '4I': number
+    '5I': number
+  }
+  productList: Array<string>
+}
 
 export function DrawerForm() {
+  const form = useForm<DrawerForm>({
+    initialValues: {
+      peoples: {
+        '1I': 1,
+        '2I': 1,
+        '3I': 1,
+        '4I': 1,
+        '5I': 1,
+      },
+      productList: [],
+    },
+  })
+
   return (
-    <form>
+    <form onSubmit={form.onSubmit((data) => console.log(data))}>
       <Stack align={'stretch'} spacing={'sm'}>
-        <FileButton onChange={() => console.log('test')}>
+        <FileButton {...form.getInputProps('productList')}>
           {(properties) => (
             <Button fullWidth {...properties}>
               import file
@@ -12,14 +37,39 @@ export function DrawerForm() {
           )}
         </FileButton>
         <Stack align={'stretch'} spacing={'sm'}>
-          <NumberInput defaultValue={1} placeholder='人数を入力' label='1Iの人数' />
-          <NumberInput defaultValue={1} placeholder='人数を入力' label='2Iの人数' />
-          <NumberInput defaultValue={1} placeholder='人数を入力' label='3Iの人数' />
-          <NumberInput defaultValue={1} placeholder='人数を入力' label='4Iの人数' />
-          <NumberInput defaultValue={1} placeholder='人数を入力' label='5Iの人数' />
+          <NumberInput
+            defaultValue={1}
+            placeholder='人数を入力'
+            label='1Iの人数'
+            {...form.getInputProps('peoples.1I')}
+          />
+          <NumberInput
+            defaultValue={1}
+            placeholder='人数を入力'
+            label='2Iの人数'
+            {...form.getInputProps('peoples.2I')}
+          />
+          <NumberInput
+            defaultValue={1}
+            placeholder='人数を入力'
+            label='3Iの人数'
+            {...form.getInputProps('peoples.3I')}
+          />
+          <NumberInput
+            defaultValue={1}
+            placeholder='人数を入力'
+            label='4Iの人数'
+            {...form.getInputProps('peoples.4I')}
+          />
+          <NumberInput
+            defaultValue={1}
+            placeholder='人数を入力'
+            label='5Iの人数'
+            {...form.getInputProps('peoples.5I')}
+          />
         </Stack>
         <Divider my={'lg'} />
-        <Button>設定</Button>
+        <Button type={'submit'}>設定</Button>
       </Stack>
     </form>
   )
