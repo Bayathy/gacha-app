9b49c7ad5ca294be2d5f84919ed13fc8ae9682d8
Bayathy <misosoup0321@icloud.com>
2023-05-09 09:34:05 +0900

    feat: re-style main page

--- src/components/ui/main/main.tsx
+++ src/components/ui/main/main.tsx
@@ -1,18 +1,23 @@
-import { Box, Button, Divider, Group, Stack, Text } from '@mantine/core'
+import { Box, Button, Group, Stack, Text } from '@mantine/core'
 
 export function Main() {
   return (
     <main className={'m-auto flex h-full w-full max-w-2xl items-center justify-center'}>
-      <Stack justify={'stretch'} className={'w-full'}>
-        <Box className={'items-center justify-center rounded-md p-8 shadow-md'}>
-          <Text className={'line-clamp-3 text-3xl'}>Product</Text>
-          <Divider py={'md'} />
-          <Text className={'text-2xl'}>番号: 1I01</Text>
+      <Stack justify={'stretch'} className={'h-full w-full'}>
+        <Box
+          className={'flex h-full flex-col items-center justify-around rounded-md p-8 shadow-md'}
+        >
+          <Stack justify={'stretch'} className={'w-full'}>
+            <Box className={'flex h-32 items-center justify-center'}>
+              <Text className={'line-clamp-3 break-words text-3xl'}>Product</Text>
+            </Box>
+            <Text className={'m-auto text-4xl'}>1I01</Text>
+          </Stack>
+          <Group grow spacing={'md'} className={'w-full'}>
+            <Button>抽選する</Button>
+            <Button>次へ</Button>
+          </Group>
         </Box>
-        <Group grow spacing={'md'}>
-          <Button>抽選する</Button>
-          <Button>次へ</Button>
-        </Group>
       </Stack>
     </main>
   )
