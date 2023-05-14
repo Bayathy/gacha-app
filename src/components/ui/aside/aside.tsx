d3fe7bccd513c7508a63aeff33e3203050e51e1d
Bayathy <misosoup0321@icloud.com>
2023-05-15 01:10:28 +0900

    feat: add fuction

--- src/components/ui/aside/aside.tsx
+++ src/components/ui/aside/aside.tsx
@@ -1,13 +1,18 @@
 import { MediaQuery, Text, Aside as MantineAside, Divider, ScrollArea } from '@mantine/core'
+import { useRecoilState } from 'recoil'
+
+import { formProductListState } from '@/components/store/form-product-list-store'
 export function Aside() {
+  const [productList] = useRecoilState(formProductListState)
   return (
     <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
       <MantineAside p='md' hiddenBreakpoint='sm' width={{ sm: 300 }}>
         <Text ta={'center'}>履歴</Text>
         <Divider />
         <ScrollArea>
-          <Text>test</Text>
-          <Text>test</Text>
+          {productList.map((index, key) => (
+            <Text key={key}>{index}</Text>
+          ))}
         </ScrollArea>
       </MantineAside>
     </MediaQuery>
--- src/components/ui/main/main.tsx
+++ src/components/ui/main/main.tsx
@@ -1,6 +1,14 @@
+import { useState } from 'react'
+
 import { Box, Button, Group, Stack, Text } from '@mantine/core'
+import { useRecoilState } from 'recoil'
+
+import { formProductListState } from '@/components/store/form-product-list-store'
 
 export function Main() {
+  const [listIndex, setListIndex] = useState<number>(0)
+  const productList = useRecoilState(formProductListState)[0]
+
   return (
     <main className={'m-auto flex h-full w-full max-w-2xl items-center justify-center'}>
       <Stack justify={'stretch'} className={'h-full w-full'}>
@@ -9,13 +17,23 @@ export function Main() {
         >
           <Stack justify={'stretch'} className={'w-full'}>
             <Box className={'flex h-32 items-center justify-center'}>
-              <Text className={'line-clamp-3 break-words text-3xl'}>Product</Text>
+              <Text className={'line-clamp-3 break-words text-3xl'}>
+                {productList.length > 0 ? productList[listIndex] : 'Please Import'}
+              </Text>
             </Box>
             <Text className={'m-auto text-4xl'}>1I01</Text>
           </Stack>
           <Group grow spacing={'md'} className={'w-full'}>
+            <Button disabled={listIndex == 0} onClick={() => setListIndex(listIndex - 1)}>
+              前へ
+            </Button>
             <Button>抽選する</Button>
-            <Button>次へ</Button>
+            <Button
+              disabled={listIndex + 1 == productList.length}
+              onClick={() => setListIndex(listIndex + 1)}
+            >
+              次へ
+            </Button>
           </Group>
         </Box>
       </Stack>
